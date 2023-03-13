dd28be37e01c14ae32154c1113705978597ed747
“Omar <ocamacho812@gmail.com>
2023-03-13 00:36:45 -0700

    Add validations to login and register

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -6,6 +6,8 @@ const User = mongoose.model('User');
 const passport = require('passport');
 const { loginUser, restoreUser } = require('../../config/passport');
 const { isProduction } = require('../../config/keys');
+const validateRegisterInput = require('../../validations/register');
+const validateLoginInput = require('../../validations/login');
 
 router.get('/', function (req, res, next) {
   res.json({
@@ -13,55 +15,59 @@ router.get('/', function (req, res, next) {
   });
 });
 
-router.post('/register', async (req, res, next) => {
-  // Check to make sure no one has already registered with the proposed email or
-  // username.
-  const user = await User.findOne({
-    $or: [{ email: req.body.email }, { username: req.body.username }],
-  });
+router.post(
+  '/register',
+  validateRegisterInput,
+  async (req, res, next) => {
+    const user = await User.findOne({
+      $or: [
+        { email: req.body.email },
+        { username: req.body.username },
+      ],
+    });
 
-  if (user) {
-    // Throw a 400 error if the email address and/or email already exists
-    const err = new Error('Validation Error');
-    err.statusCode = 400;
-    const errors = {};
-    if (user.email === req.body.email) {
-      errors.email = 'A user has already registered with this email';
-    }
-    if (user.username === req.body.username) {
-      errors.username =
-        'A user has already registered with this username';
-    }
-    err.errors = errors;
-    return next(err);
-  }
-
-  // Otherwise create a new user
-  const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-  });
-
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) throw err;
-    bcrypt.hash(
-      req.body.password,
-      salt,
-      async (err, hashedPassword) => {
-        if (err) throw err;
-        try {
-          newUser.hashedPassword = hashedPassword;
-          const user = await newUser.save();
-          return res.json({ user });
-        } catch (err) {
-          next(err);
-        }
+    if (user) {
+      const err = new Error('Validation Error');
+      err.statusCode = 400;
+      const errors = {};
+      if (user.email === req.body.email) {
+        errors.email =
+          'A user has already registered with this email';
       }
-    );
-  });
-});
+      if (user.username === req.body.username) {
+        errors.username =
+          'A user has already registered with this username';
+      }
+      err.errors = errors;
+      return next(err);
+    }
 
-router.post('/login', async (req, res, next) => {
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+    });
+
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) throw err;
+      bcrypt.hash(
+        req.body.password,
+        salt,
+        async (err, hashedPassword) => {
+          if (err) throw err;
+          try {
+            newUser.hashedPassword = hashedPassword;
+            const user = await newUser.save();
+            return res.json({ user });
+          } catch (err) {
+            next(err);
+          }
+        }
+      );
+    });
+  }
+);
+
+router.post('/login', validateLoginInput, async (req, res, next) => {
   passport.authenticate('local', async function (err, user) {
     if (err) return next(err);
     if (!user) {
@@ -70,15 +76,12 @@ router.post('/login', async (req, res, next) => {
       err.errors = { email: 'Invalid credentials' };
       return next(err);
     }
-    return res.json(await loginUser(user)); // <-- THIS IS THE CHANGED LINE
+    return res.json(await loginUser(user));
   })(req, res, next);
 });
 
 router.get('/current', restoreUser, (req, res) => {
   if (!isProduction) {
-    // In development, allow React server to gain access to the CSRF token
-    // whenever the current user information is first loaded into the
-    // React application
     const csrfToken = req.csrfToken();
     res.cookie('CSRF-TOKEN', csrfToken);
   }
